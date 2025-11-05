903457df741abcb7cbacb5ef85effb845cf81f49
zie rou <zierou@proton.me>
2025-11-05 15:17:30 -0500

    touch up popup

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import logo from '@assets/img/logo.svg';
 
-export default function Popup() {
-
+/*
 let createData = {
 //  type: "detached_panel",
   url: "src/pages/content/main/index.html",
@@ -10,14 +9,23 @@ let createData = {
   height: 100,
 };
 //let creating = browser.tabs.create(createData);
+*/
 
-  return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
 
-        <button onClick={()=>browser.tabs.create({'url':'/src/pages/main/index.html'})}>open reader</button>
-      </header>
-    </div>
-  );
+export default function Popup() {
+    const url = '/src/pages/main/index.html'
+    return (
+        <div className={`absolute top-0 left-0 right-0 bottom-0 text-center
+            h-full p-3 bg-zinc-800 grid`}>
+            <header className={`place-self-center flex flex-col gap-y-3
+                items-center justify-center text-white`}>
+                <img className="h-32 pointer-events-none" alt="logo"
+                    src={logo} />
+                <button
+                    className='border py-3 px-1 bg-zinc-600 hover:bg-yellow-300/50'
+                    onClick={() => { return browser.tabs.create({url}) }}>
+                    Open Graze</button>
+            </header>
+        </div>);
 }
+
