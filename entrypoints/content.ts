8d8e35b4d60819a21a17c2f9b4502f32fed3bc7b
zie rou <zierou@proton.me>
2025-11-26 22:19:11 -0500

    integrate entrypoint content

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,33 @@
+import { getParagraphsWithHeadings } from "@/utils/xpathUtils.tsx"
+
+
+
+/*
+let paragraphNodes = document.getElementsByTagName('p')
+let _paragraphs = Array.from(paragraphNodes).map((el)=>{
+    return el.innerText
+});
+*/
+
+
+
+
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
+  matches: ['<all_urls>'],
   main() {
-    console.log('Hello content.');
+    try {
+      console.log('content script loaded');
+    } catch (e) {
+      console.error(e);
+    }
+    browser.runtime.onMessage.addListener((request) => {
+        console.log("Message from the background script:");
+        console.log(request.greeting);
+        const structuredWork = getParagraphsWithHeadings(window)
+        return Promise.resolve({
+            response: "Hi from content script",
+            structuredWork
+        });
+    });
   },
 });
