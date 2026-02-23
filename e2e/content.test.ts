cae1b2c4ddc0359c8b7c4f3b22a45e2ea4119e71
zie rou <zierou@proton.me>
2026-02-23 15:00:32 -0500

    e2e scaffold content tests by front-end

--- e2e/content.test.ts
+++ e2e/content.test.ts
@@ -26,69 +26,77 @@ test.describe('content script and dependent features', () => {
     await mainPage.clickDismissButton()
   })
 
-  // TODO we are checking by proxy maybe chech external console or mach
-  test("Content script appears in open tabs list", async ({ page, extensionId, context }) => {
-    const externalTitle = await externalPage.title()
-    const openTable = await mainPage.getOpenTabsTable()
-    const openTabsText = await openTable.textContent()
-    expect(openTabsText.includes(externalTitle)).toBe(true)
+  test.describe('main extension page', ()=>{
+    // TODO we are checking by proxy maybe chech external console or mach
+    test("Content script appears in open tabs list", async ({ page, extensionId, context }) => {
+      const externalTitle = await externalPage.title()
+      const openTable = await mainPage.getOpenTabsTable()
+      const openTabsText = await openTable.textContent()
+      expect(openTabsText.includes(externalTitle)).toBe(true)
+    });
+  })
+
+  test.describe('graze-main-text (midsection)', () => {
+    test("reader contains string from content script", async ({ page, extensionId, context }) => {
+      const matchCase1 = 'TypeScript'
+      const externalTitle = await externalPage.title()
+      await mainPage.clickReaderButton(externalTitle)
+
+      const mainTextDiv = await mainPage.getReaderMainTextDiv()
+      let mainText = await mainTextDiv.textContent()
+      expect(mainText.length).toBeGreaterThan(100)
+      expect(mainText).toContain(matchCase1)
+      /*
+      // TODO check against article 
+      const externalBody = await externalPage.locator('body').innerText()
+      for (const word of mainText) {
+        console.log(word)
+        expect(externalBody.includes(word)).toBeTruthy
+      }
+      */
+    });
+
+    test("reader nav controls location", async ({ page, extensionId, context }) => {
+      const externalTitle = await externalPage.title()
+      await mainPage.clickReaderButton(externalTitle)
+
+      const mainTextDiv = await mainPage.getReaderMainTextDiv()
+      const mainText1 = await mainTextDiv.textContent()
+
+      await mainPage.clickReaderNavNextButton()
+
+      const mainText2 = await mainTextDiv.textContent()
+
+      expect(mainText1 === mainText2).toBeFalsy()
+
+      await mainPage.clickReaderNavPrevButton()
+      const mainText3 = await mainTextDiv.textContent()
+
+      expect(mainText3 === mainText1).toBeTruthy()
+    });
   });
+  
+  test.describe("reader toolbar bottom adjusts...", ()=> {
+    test("reader location by charIndex", async ({ page, extensionId, context }) => {
+      const cIdx = 20;
+      const externalTitle = await externalPage.title()
+      await mainPage.clickReaderButton(externalTitle)
 
-  test("reader contains string from content script", async ({ page, extensionId, context }) => {
-    const matchCase1 = 'TypeScript'
-    const externalTitle = await externalPage.title()
-    await mainPage.clickReaderButton(externalTitle)
+      const mainTextDiv = await mainPage.getReaderMainTextDiv()
+      const mainText1 = await mainTextDiv.textContent()
 
-    const mainTextDiv = await mainPage.getReaderMainTextDiv()
-    let mainText = await mainTextDiv.textContent()
-    expect(mainText.length).toBeGreaterThan(100)
-    expect(mainText).toContain(matchCase1)
-    /*
-    // TODO check against article 
-    const externalBody = await externalPage.locator('body').innerText()
-    for (const word of mainText) {
-      console.log(word)
-      expect(externalBody.includes(word)).toBeTruthy
-    }
-    */
-  });
+      await mainPage.fillReaderCharIndexInput(cIdx)
+      await mainPage.clickSubmitLocation()
 
-  test("reader nav controls location", async ({ page, extensionId, context }) => {
-    const externalTitle = await externalPage.title()
-    await mainPage.clickReaderButton(externalTitle)
+      const mainText2 = await mainTextDiv.textContent()
+      expect(mainText1 === mainText2).toBeFalsy()
 
-    const mainTextDiv = await mainPage.getReaderMainTextDiv()
-    const mainText1 = await mainTextDiv.textContent()
-
-    await mainPage.clickReaderNavNextButton()
-
-    const mainText2 = await mainTextDiv.textContent()
-
-    expect(mainText1 === mainText2).toBeFalsy()
-
-    await mainPage.clickReaderNavPrevButton()
-    const mainText3 = await mainTextDiv.textContent()
-
-    expect(mainText3 === mainText1).toBeTruthy()
-  });
-
-  test("reader changes location by charIndex", async ({ page, extensionId, context }) => {
-    const cIdx = 20;
-    const externalTitle = await externalPage.title()
-    await mainPage.clickReaderButton(externalTitle)
-
-    const mainTextDiv = await mainPage.getReaderMainTextDiv()
-    const mainText1 = await mainTextDiv.textContent()
-
-    await mainPage.fillReaderCharIndexInput(cIdx)
-    await mainPage.clickSubmitLocation()
-
-    const mainText2 = await mainTextDiv.textContent()
-    expect(mainText1 === mainText2).toBeFalsy()
-
-    const progressBar = await mainPage.getReaderProgressBar()
-    const progressText = await progressBar.textContent()
-    expect(progressText).toContain(`${cIdx}`)
+      const progressBar = await mainPage.getReaderProgressBar()
+      const progressText = await progressBar.textContent()
+      expect(progressText).toContain(`${cIdx}`)
+    });
+    test('mainText charlength', async ({}) => {});
+    test('mainText fontSize', async ({})=>{});
   });
 })
 
